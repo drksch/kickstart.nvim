8d4df7b552e498ff09ed72899fd8fb96e7bc7f47
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-11-25 04:20:45 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -565,14 +565,14 @@ require('lazy').setup({
         -- ts_ls = {},
         --
 
-        lua_ls = {
+       -- lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
           -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
+          --settings = {
+            --Lua = {
+              --completion = {
+                --callSnippet = 'Replace',
               },
               -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
               -- diagnostics = { disable = { 'missing-fields' } },
