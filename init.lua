bbc1fe91c820e85a349388b101b79ad4dfb1110f
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-11-25 06:02:32 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -1007,8 +1007,42 @@ require('lazy').setup({
 
   { --For School Writing Test1: Markdown
       'folke/zen-mode.nvim',
-      opts = {}, 
-  },
+      opts = {},
+      require("zen-mode").setup({
+        window = {
+          backdrop = 0.95, 
+          width = 120, -- width of the Zen window
+          height = 1, -- height of the Zen window
+          options = {
+            signcolumn = "no", -- disable signcolumn
+            number = false, -- disable number column
+            relativenumber = false, -- disable relative numbers
+            -- cursorline = false, -- disable cursorline
+            -- cursorcolumn = false, -- disable cursor column
+            -- foldcolumn = "0", -- disable fold column
+            -- list = false, -- disable whitespace characters
+          },
+        },
+        plugins = {
+          -- disable some global vim options (vim.o...)
+          options = {
+            enabled = true,
+            ruler = true, -- disables the ruler text in the cmd line area
+            showcmd = false, -- disables the command in the last line of the screen
+            -- you may turn on/off statusline in zen mode by setting 'laststatus' 
+            -- statusline will be shown only if 'laststatus' == 3
+            laststatus = 0, -- turn off the statusline in zen mode
+          },
+          twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+          gitsigns = { enabled = true }, -- enables git signs
+          tmux = { enabled = false }, -- disables the tmux statusline
+          wezterm = {
+            enabled = true,
+            font = "+20", -- (10% increase per step)
+          },
+        },
+      }) 
+  },  
 
   { --For School Writing Test2: Markdown
       'folke/twilight.nvim',
