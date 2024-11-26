e4496f5ac41892b6dcc7364bcc6d7025d656f907
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2024-11-27 02:36:30 +0800

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -57,6 +57,6 @@ opts ={},
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-}.
+},
 
 }
