7730540982b2ec239debc922f0b47c6e53a06646
Devlin Nightshade <163310407+drksch@users.noreply.github.com>
2025-11-24 02:33:28 +0800

    Update init.lua

--- init.lua
+++ init.lua
@@ -893,7 +893,24 @@ require('lazy').setup({
       vim.cmd.colorscheme 'kanagawa-paper'
     end,
   },
+   {
+    'rebelot/kanagawa.nvim',
+    lazy = false
+    priority = 1000,
+    init = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('kanagawa').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
 
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
